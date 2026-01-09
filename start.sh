479cb52d88072d0242614599349aa5384264f0b8
Alongbar Basumatary <155052855+alongbarbasumatary@users.noreply.github.com>
2026-01-09 07:08:38 +0530

    Update start.sh to change terminal startup message

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-echo "Starting Web Terminal..."
-
-ttyd \
-  --writable \
-  --port 10000 \
-  --interface 0.0.0.0 \
-  /bin/bash
+echo "Starting Ubuntu Web Terminal..."
+ttyd --writable --port 10000 --interface 0.0.0.0 /bin/bash
